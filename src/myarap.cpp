9c849c6358a7ce81c57243f3b26d8c8e8d4e3e9a
dralois <epple.alexander@web.de>
2022-02-05 15:27:45 +0100

    Changed glad to glew & optimized project
    - Improved cmake further
    - Changed from glad to glew for ease of use
    - Removed unnecessary files
    - Fixed folder structure (shaders & resources)
    - Improved includes

---
+++ src/myarap.cpp
@@ -0,0 +1,17 @@
+#include "../include/myarap.h"
+
+
+/*
+void MYARAP::use(std::vector<glm::vec3> vertices, std::vector<unsigned int> ind, std::vector<int> anchorpoints) {
+	
+}
+void MYARAP::simpleARAP() {
+
+}
+void MYARAP::skeletonARAP() {
+
+}
+void MYARAP::cageARAP() {
+
+
+}*/

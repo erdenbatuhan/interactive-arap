fcd2768251064c3fb1c60f749f1702f0b32e9c98
dralois <epple.alexander@web.de>
2022-02-09 12:45:47 +0100

    Updating from master & tabs -> spaces

--- include/Arap-Ceres.h
+++ include/Arap-Ceres.h
@@ -11,25 +11,27 @@
 
 #include <map>
 #include <vector>
+#include <utility>
 
 #include <ceres/ceres.h>
 #include <ceres/rotation.h>
 
-class Arap {
+class Arap
+{
 public:
   Arap();
   ~Arap() = default;
 
-  void updateParameters(int);
+  void updateMovingVertex(int, const Eigen::Vector3f &);
 
-  std::vector<int> collectFixedVertices(Eigen::MatrixXi&, std::vector<int>&) const;
-  void runDeformation(Eigen::MatrixXd&, Eigen::MatrixXi&, std::vector<int>&, std::map<int, std::vector<int>>&) const;
+  std::vector<int> collectFixedVertices(Eigen::MatrixXi &, const std::vector<int> &) const;
+  Eigen::MatrixXd computeDeformation(Eigen::MatrixXd &, Eigen::MatrixXi &,
+                                     std::map<int, std::vector<int>> &, const std::vector<int> &);
+
 private:
-  // Neighborhood of vertices (Mapping between vertex id and its neighbor ids)
-  const std::map<int, std::vector<int>> m_neighborhood;
-
-  // The current moving vertex
-  int m_movingVertex{};
+     // The current moving vertex
+    int m_movingVertex{};
+    Eigen::Vector3d m_movingVertexPosition{};
 };
 
 #endif // _ARAP_CERES_H_

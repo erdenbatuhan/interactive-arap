0a95372a8d615a369120059772e35288baed9b8d
Batuhan Erden <erden.batuhan@gmail.com>
2022-02-10 07:07:35 +0100

    set default weighting to constant

--- include/Arap.h
+++ include/Arap.h
@@ -23,7 +23,7 @@
 #pragma omp declare reduction(merge: std::vector<Eigen::Matrix3d>: omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
 #endif
 
-#define USE_COTANGENT_WEIGHTS 1 // Otherwise, constant weights will be applied
+#define USE_COTANGENT_WEIGHTS 0 // Otherwise, constant weights will be applied
 #define NUM_ITERATIONS 4
 #define LOWER_ENERGY_THRESHOLD 1e-5
 
